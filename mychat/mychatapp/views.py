06fb372ea7d79f4eca0cfa56c855912024c53fc5
ankita kumari <ankitachaturvedi890@gmail.com>
2023-03-02 00:25:38 +0530

    first commit

---
+++ mychat/mychatapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Profile, Friend
+# Create your views here.
+
+
+def index(request):
+    user = request.user.profile
+    friends = user.friends.all()
+    context = {"user": user, "friends": friends}
+    return render(request, "mychatapp/index.html", context)
+
+
+def detail(request, pk):
+    friend = Friend.objects.get(profile_id=pk)
+    context = {"friend": friend}
+    return render(request, "mychatapp/detail.html", context)
